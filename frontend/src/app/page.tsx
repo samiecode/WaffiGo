c4ddb079cd105ca5e36cb3df4a1ac1ea6bc59e43
samiecode <samiekeyz02@gmail.com>
2025-11-26 14:59:30 +0100

    feat: Simplify Home component by removing unused imports and logic, retaining only WalletDashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,7 @@
 "use client";
 
-import {Dashboard} from "@/components/pages/dashboard";
-import {useWallet} from "@/hooks/use-wallet";
-import {CELO_MAINNET, CELO_SEPOLIA} from "@/lib/contract";
-import {WalletConnect} from "@/components/wallet-connect";
-import {AppLayout} from "@/components/app-layout";
-import { WalletDashboard } from "@/components/wallet-dashboard"
+import {WalletDashboard} from "@/components/wallet-dashboard";
 
 export default function Home() {
-	// const {isConnected, chainId} = useWallet();
-
-	// const isCeloNetwork =
-	// 	chainId === CELO_MAINNET.chainId || chainId === CELO_SEPOLIA.chainId;
-
-	// if (!isConnected || !isCeloNetwork) {
-	// 	return <WalletConnect />;
-	// }
-	// return (
-	// 	<AppLayout>
-	// 		<Dashboard />
-	// 	</AppLayout>
-	// );
 	return <WalletDashboard />;
 }
